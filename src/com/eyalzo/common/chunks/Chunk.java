d6901ad502ddfd41fbe5798b428c5eb190df036d
Aran Bergman <aran@yahoo-inc.com>
2014-11-30 14:19:29 +0200

    Added documentation to Chunk class.

--- src/com/eyalzo/common/chunks/Chunk.java
+++ src/com/eyalzo/common/chunks/Chunk.java
@@ -4,29 +4,59 @@
 package com.eyalzo.common.chunks;
 
 /**
+ * Chunk class is a variation on the chunks as represented by a long primitive data type (or its Long wrapper), to create a more OO design. 
+ * Can later be replaced by the "long" version due to speed requirements, while keeping the OO design.
+ * 
  * @author Aran Bergman
  *
  */
 public class Chunk {
+	/**
+	 * Length of the chunk in bytes.
+	 */
 	private int length;
+	/**
+	 * The signature of the chunk.
+	 */
 	private long signature;
+	/**
+	 * The offset of this chunk in the original text.
+	 */
 	private int startOffset;
 	
+	/**
+	 * Create a chunk with the specified parameters.
+	 * 
+	 * @param length
+	 * @param signature
+	 * @param startOffset
+	 */
 	public Chunk(int length, long signature, int startOffset) {
 		this.length = length;
 		this.signature = signature;
 		this.startOffset = startOffset;
 	}
 	
+	/**
+	 * @return the  "code" of the chunk - i.e., its length combined with its signature. Can be used to compare chunks (prefer the {@link #equals} method).
+	 */
 	public long getCode() {
 		long result = ((length & PackChunking.CHUNK_LEN_MASK) << PackChunking.CHUNK_SHA1_BITS) | (PackChunking.CHUNK_SHA1_MASK & signature);
 		return result;
 	}
 
+	/** 
+	 * 
+	 * @return chunk length in bytes.
+	 */
 	public int getLength() {
 		return length;
 	}
 
+	/**
+	 * 
+	 * @return The offset of the chunk in the original text.
+	 */
 	public int getStartOffset() {
 		return startOffset;
 	}
